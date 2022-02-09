36608f3bae7cfaa5d89bff1d8ac7fec7144e54ca
kevin <xingnaili14@gmail.com>
2022-02-09 20:06:31 +0800

    Add birthAvg aggregator

---
+++ src/main/java/com/nus/cool/core/cohort/aggregator/BirthAvgAggregator.java
@@ -0,0 +1,45 @@
+package com.nus.cool.core.cohort.aggregator;
+
+
+import com.nus.cool.core.cohort.TimeUnit;
+import com.nus.cool.core.cohort.filter.FieldFilter;
+import com.nus.cool.core.io.storevector.InputVector;
+
+import java.util.BitSet;
+import java.util.List;
+import java.util.Map;
+
+public class BirthAvgAggregator implements EventAggregator {
+
+    private InputVector values;
+
+    @Override
+    public void init(InputVector vec) {
+        values = vec;
+    }
+
+    @Override
+    public Double birthAggregate(List<Integer> offset) {
+    	if (offset.isEmpty()) return null;
+        double r = 0;
+        for (Integer i : offset) {
+            r += values.get(i);
+        }
+        return r/offset.size();
+    }
+
+	@Override
+	public void ageAggregate(BitSet ageOffset, BitSet ageDelimiter, int start, int end, int ageInterval,
+                             FieldFilter ageFilter, Map<Integer, List<Double>> ageMetrics) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ageAggregate(BitSet ageOffset, InputVector time, int birthDay, int ageOff, int ageEnd, int ageInterval,
+                             TimeUnit unit, FieldFilter ageFilter, Map<Integer, List<Double>> ageMetrics) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
