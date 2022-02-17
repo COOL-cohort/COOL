94c01d6ad4b19cd4ea531c6591ea5a1552e7f392
Qingpeng Cai <qingpeng@u.nus.edu>
2022-02-18 01:04:39 +0800

    Debug query server

--- cool-queryserver/src/main/java/com.nus.cool.queryserver/QueryServer.java
+++ cool-queryserver/src/main/java/com.nus.cool.queryserver/QueryServer.java
@@ -1,24 +1,33 @@
 package com.nus.cool.queryserver;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
+import org.eclipse.jetty.server.*;
+import org.eclipse.jetty.server.handler.HandlerCollection;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.servlet.ServletContainer;
 
-
-@Path("/")
 public class QueryServer {
-    @Path("hello")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    QueryResult reload(@QueryParam("name") String name){
-        return
-    }
+    public static void main(String[] args) throws Exception{
+        Server server = new Server(8080);
 
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+
+        // 配置Servlet
+        ServletHolder holder = context.addServlet(ServletContainer.class, "/*");
+//        holder.setInitOrder(1);
+        holder.setInitParameter("jersey.config.server.provider.packages", "com.nus.cool.queryserver");
+
+        try {
+            server.start();
+            server.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            server.destroy();
+        }
+
+    }
 }
--- cool-queryserver/src/main/java/com.nus.cool.queryserver/QueryServerController.java
+++ cool-queryserver/src/main/java/com.nus.cool.queryserver/QueryServerController.java
@@ -8,20 +8,35 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.eclipse.jetty.server.Request;
 
-import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class QueryServerController implements QueryServerAPI {
+import javax.ws.rs.core.MediaType;
 
-	public QueryServerController() {
+@Path("v1")
+public class QueryServerController {
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getM() {
+		return "My message\n";
 	}
 
-	@Override
-	public QueryResult reload(String name) {
+	@Path("msg")
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getMessage() {
+		return "My message\n";
+	}
+
+	@Path("name")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String name(@QueryParam("name") String name){
 		System.out.println(name);
-		return QueryResult.ok();
+		return name;
 	}
 }
