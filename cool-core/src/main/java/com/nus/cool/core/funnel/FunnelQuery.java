583c96632b8ac86824f98beee6112860d82b418e
Qingpeng Cai <qingpeng@u.nus.edu>
2022-03-26 22:42:02 +0800

    Add funnel query

---
+++ cool-core/src/main/java/com/nus/cool/core/funnel/FunnelQuery.java
@@ -0,0 +1,49 @@
+package com.nus.cool.core.funnel;
+
+import com.nus.cool.core.cohort.BirthSequence;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FunnelQuery {
+	
+	private String dataSource;
+	
+	private String inputCohort;
+	
+	private List<BirthSequence> stages = new ArrayList<>();
+	
+	boolean isOrdered = true;	
+	
+	public List<BirthSequence> getStages() {
+		return stages;
+	}
+
+	public void setStages(List<BirthSequence> stages) {
+		this.stages = stages;
+	}
+
+	public boolean isOrdered() {
+		return isOrdered;
+	}
+
+	public void setOrdered(boolean isOrdered) {
+		this.isOrdered = isOrdered;
+	}
+
+	public String getDataSource() {
+		return dataSource;
+	}
+
+	public void setDataSource(String dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	public String getInputCohort() {
+		return inputCohort;
+	}
+
+	public void setInputCohort(String inputCohort) {
+		this.inputCohort = inputCohort;
+	}
+}
