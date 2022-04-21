f1998a5d531ddb5707f3046a434b43a78c0d0f6a
Qingpeng Cai <qingpeng@u.nus.edu>
2022-04-21 20:10:13 +0800

    remove the dimension file from the load function in cool-extension

--- cool-extensions/arrow-extensions/src/main/java/com/nus/cool/extension/model/ArrowLoader.java
+++ cool-extensions/arrow-extensions/src/main/java/com/nus/cool/extension/model/ArrowLoader.java
@@ -11,19 +11,18 @@ public class ArrowLoader {
      * Please list the necessary dataset files for the COOL system to load into a new cube.
      * @param args there are five arguments. List in input order
      *  (1) output cube name: to be specified when loading from the repository
-     *  (2) table.yaml (3) dimension.csv (4) data.arrow (5) output cube repository
+     *  (2) table.yaml (3) data.arrow (4) output cube repository
      */
     public static void main(String[] args) {
         String cube = args[0];
         String schemaFileName = args[1];
-        String dimensionFileName = args[2];
         String dataFileName = args[3];
         String cubeRepo = args[4];
 
         try {
             DataLoaderConfig config = new ArrowIPCFileDataLoaderConfig();
             CoolLoader coolLoader = new CoolLoader(config);
-            coolLoader.load(cube,schemaFileName,dimensionFileName,dataFileName,cubeRepo);
+            coolLoader.load(cube,schemaFileName,dataFileName,cubeRepo);
         } catch (IOException e){
             System.out.println("Failed to load data");
             System.out.println(e);
