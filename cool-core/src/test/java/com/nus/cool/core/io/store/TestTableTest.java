6b1063aa7dbdb56a4eef07ea6f59083ab4d74de8
Zrealshadow <704309740@qq.com>
2022-04-23 20:29:42 +0800

    fix TestData path

--- cool-core/src/test/java/com/nus/cool/core/io/store/TestTableTest.java
+++ cool-core/src/test/java/com/nus/cool/core/io/store/TestTableTest.java
@@ -1,19 +1,34 @@
 package com.nus.cool.core.io.store;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
+
 
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class TestTableTest {
+
+    private String sourcePath;
     @BeforeTest
     public void setUp() {
         System.out.println("Start To Test TestTable");
+        sourcePath = Paths.get(System.getProperty("user.dir"),
+             "src",
+             "test",
+             "java",
+             "com",
+             "nus",
+             "cool",
+             "core",
+             "resources").toString();
+        System.out.println(sourcePath);
     }
 
     @Test
     public void BasicPrintTest() {
-        String filepath = Paths.get("../TestData/test.csv").toAbsolutePath().toString();
+        String filepath = Paths.get(sourcePath,"TestData","test.csv").toString();
+        System.out.println(filepath);
         TestTable table = TestTable.readFromCSV(filepath);
         table.ShowTableHead();
     }
