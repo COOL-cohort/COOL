f224ae12489a1c87b5b23605bdc9fb300734cdfb
liuchangshiye <liuchangshiye@gmail.com>
2022-05-23 10:39:24 +0800

    add gettr to invariant

--- cool-core/src/main/java/com/nus/cool/core/schema/FieldSchema.java
+++ cool-core/src/main/java/com/nus/cool/core/schema/FieldSchema.java
@@ -22,6 +22,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 /**
@@ -47,6 +48,7 @@ public class FieldSchema {
 
   private boolean preCal;
 
+  @Getter
   // whether the personal information
   private boolean invariant;
 
@@ -56,6 +58,8 @@ public class FieldSchema {
   // public String getAggregator() {
   //   return aggregator;
   // }
-
+  public boolean getInvariant(){
+    return invariant;
+  }
 
 }
