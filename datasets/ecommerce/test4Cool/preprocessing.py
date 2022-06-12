7d027a6865fd09fdcf173dd93818ce3c7e556954
NLGithubWP <xingnaili14@gmail.com>
2022-06-12 23:54:36 +0800

    Revise the sql and add slides

--- datasets/ecommerce/test4Cool/preprocessing.py
+++ datasets/ecommerce/test4Cool/preprocessing.py
@@ -4,6 +4,9 @@
 
 import pandas as pd
 
-df = pd.read_csv('data.csv')
+df = pd.read_csv('/Users/kevin/project_java/COOL/datasets/ecommerce/test4Cool/cool-ecommerce.csv')
 
-print(df.to_string())
+df['Str_Reported_Date'] = df['Reported_Date'].map(lambda x: "-".join(x.split("-")[:2]))
+
+
+df.to_csv('/Users/kevin/project_java/COOL/datasets/ecommerce/test4Cool/cool-ecommerce_dup1.csv', index=False)
