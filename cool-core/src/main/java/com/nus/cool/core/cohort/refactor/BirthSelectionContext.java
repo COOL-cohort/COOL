e25bf64cd318405747e0aa90213f378b456ac47f
Zrealshadow <704309740@qq.com>
2022-06-14 18:45:29 +0800

    format

--- cool-core/src/main/java/com/nus/cool/core/cohort/refactor/BirthSelectionContext.java
+++ cool-core/src/main/java/com/nus/cool/core/cohort/refactor/BirthSelectionContext.java
@@ -1,24 +1,88 @@
 package com.nus.cool.core.cohort.refactor;
 
-import java.util.BitSet;
 import java.util.Calendar;
 import java.util.HashMap;
-import java.util.Queue;
 
-import javafx.util.Pair;
+import com.google.common.base.Preconditions;
 
 /**
  * for every user, we maintain a queue to record timewindow chosen event
  */
 public class BirthSelectionContext {
-  private BitSet userChosen;
-  private HashMap<Integer, Calendar> userBirthTime;
 
-  private class BirthContextWindow{
-        // private Queue<Pair<int,>> q;
+  private HashMap<Integer, Calendar> userSelected; // If user is selected and BirthEvent Calender
+  private HashMap<Integer, BirthContextWindow> userBirthTime; // UserId -> BirthContextWindow
+  private final TimeWindow maxTimeWindow;
+  private final int[] eventMinFrequency; // EventId (index) -> Frequency (value)
+
+  public BirthSelectionContext(TimeWindow tWindow, int[] freq) {
+    this.maxTimeWindow = tWindow;
+    this.userSelected = new HashMap<>();
+    this.userBirthTime = new HashMap<>();
+    this.eventMinFrequency = freq;
   }
-    // private HashMap<int, ArrayList>
+
+  /**
+   * Add a event which is pass the eventSelection
+   * 
+   * @param userId
+   * @param EventId
+   * @param date
+   */
+  public void Add(Integer userId, Integer eventId, Calendar date) {
+    Preconditions.checkArgument(userSelected.containsKey(userId) == false,
+        "Before Invoke Add, the userId should be unselected state");
+    if (!userBirthTime.containsKey(userId))
+      this.userBirthTime.put(userId, new BirthContextWindow(maxTimeWindow, this.eventMinFrequency.length));
+    // Initialize a BirthContextWindow
+    BirthContextWindow contextWindow = this.userBirthTime.get(userId);
+    contextWindow.put(eventId, date);
+
+    // check whether the contextWindow meet the MinFrequency
+    // if so, update the event birth date.
+    if (IsSatisfied(contextWindow.getEventState())) {
+      // If Satisfied, we update the birthEventTime
+      // the new added event make the ContextWindow satisfy the requirement
+      // means the new added event's date is the "birth Time"
+      userSelected.put(userId, date);
+      userBirthTime.remove(userId);
+      // free the context content for selected user.
+    }
+  }
+
+  /**
+   * Judge whether the user's birthEvent is selected
+   * 
+   * @param userId
+   * @return
+   */
+  public boolean IsUserSelected(Integer userId) {
+    return userSelected.containsKey(userId);
+  }
+
+  /**
+   * get the Calender of birthEvent date of certain user
+   * 
+   * @param userId
+   * @return null if user is not selected
+   */
+  public Calendar getUserBirthEventDate(Integer userId) {
+    return userSelected.get(userId);
+  }
+
+  /**
+   * check whether in ContextWindow the birthEvent requirement is satisfied.
+   * 
+   * @param eventState
+   * @return
+   */
+  private boolean IsSatisfied(int[] eventState) {
+    for (int i = 0; i < eventMinFrequency.length; i++) {
+      if (eventState[i] < eventMinFrequency[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
 }
-
-
-
--- cool-core/src/main/java/com/nus/cool/core/cohort/refactor/EventSelection.java
+++ cool-core/src/main/java/com/nus/cool/core/cohort/refactor/EventSelection.java
@@ -1,37 +1,32 @@
 package com.nus.cool.core.cohort.refactor;
 
 import java.util.ArrayList;
-import java.util.BitSet;
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.google.common.base.Preconditions;
-import com.nus.cool.core.io.readstore.DataFieldRS;
 
 import lombok.Getter;
 
 /**
- *  EventSelection is a collection of filters
- *  json mapper to constuct EventSelection
+ * EventSelection is a collection of filters
+ * json mapper to constuct EventSelection
  */
 public class EventSelection {
-    
+
     private ArrayList<FilterLayout> filters;
 
     @JsonIgnore
-    private  ArrayList<Filter> filterList;
-
+    private ArrayList<Filter> filterList;
 
     // Help Wrapper to input ordered tuple
     @JsonIgnore
     @Getter
     private ArrayList<String> schemaList;
-    
+
     /**
      * Create Filter from FilterLayout
      */
     public void init() {
-        for(FilterLayout layout : filters){
+        for (FilterLayout layout : filters) {
             filterList.add(layout.generateFilter());
             schemaList.add(layout.getFieldSchema());
         }
@@ -46,14 +41,16 @@ public class EventSelection {
      * @param ProjectTuple, row of one Tuple
      * @return whether this item can be chosen as a birthEvents
      */
-    public boolean Accept(Object[] ProjectTuple){
+    public boolean Accept(Object[] ProjectTuple) {
         Preconditions.checkArgument(ProjectTuple.length == schemaList.size(),
-            "Input ProjectedCol's size is equal to schemaList size");
-        for(int i = 0 ; i < ProjectTuple.length; i++){
-            if(filterList.get(i).getType() == FilterType.Set) {
-                if (!filterList.get(i).accept((String)ProjectTuple[i])) return false;
+                "Input ProjectedCol's size is equal to schemaList size");
+        for (int i = 0; i < ProjectTuple.length; i++) {
+            if (filterList.get(i).getType() == FilterType.Set) {
+                if (!filterList.get(i).accept((String) ProjectTuple[i]))
+                    return false;
             } else {
-                if (!filterList.get(i).accept((Integer)ProjectTuple[i])) return false;
+                if (!filterList.get(i).accept((Integer) ProjectTuple[i]))
+                    return false;
             }
         }
         return true;
