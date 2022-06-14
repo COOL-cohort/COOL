954ed923234aaab253a4911364045a5a1a4e928d
Zrealshadow <704309740@qq.com>
2022-06-14 18:45:29 +0800

    format

---
+++ cool-core/src/main/java/com/nus/cool/core/cohort/refactor/BirthContextWindow.java
@@ -0,0 +1,79 @@
+package com.nus.cool.core.cohort.refactor;
+
+import java.util.Calendar;
+import java.util.LinkedList;
+
+import com.google.common.base.Preconditions;
+
+import javafx.util.Pair;
+import lombok.Getter;
+
+public class BirthContextWindow {
+    private LinkedList<EventTime> window; // store EventTime in a queue, in time order
+
+    @Getter
+    private int[] eventState; // store the frequency of chosen Event in this Window
+    private final int eventNum;
+    private final TimeWindow maxTimeWindow;
+
+    public BirthContextWindow(TimeWindow tWindow, int eventNum) {
+        this.maxTimeWindow = tWindow;
+        this.eventState = new int[eventNum];
+        this.eventNum = eventNum;
+        this.window = new LinkedList<>();
+    }
+
+    /**
+     * Put new Chosen Event and TimeInto Queue
+     * 
+     * @param newEventTime
+     */
+    public void put(Integer eventId, Calendar date) {
+        Preconditions.checkState(eventId < this.eventNum,
+                "Input eventId is out of range");
+        EventTime newEventTime = new EventTime(eventId, date);
+        window.add(newEventTime);
+        this.eventState[eventId] += 1;
+
+        while (true) {
+            EventTime peak = window.peek();
+            TimeWindow duration = DateUtils.getDifference(peak.getTimeCalendar(), newEventTime.getTimeCalendar(),
+                    maxTimeWindow.getUnit());
+            if (duration.compareTo(maxTimeWindow) > 0) {
+                // pop out this Event
+                this.pop();
+            } else
+                break;
+        }
+
+    }
+
+    /**
+     * Pop out the first element in queue and update the EventState
+     */
+    public void pop() {
+        if (window.isEmpty())
+            return;
+        EventTime peak = window.peek();
+        window.remove();
+        // update eventState , default HashMap contain peak.key
+        this.eventState[peak.getEventId()] -= 1;
+    }
+
+    /**
+     * Typedef a Pair<EventId, DateTime>
+     */
+    private class EventTime extends Pair<Integer, Calendar> {
+        public EventTime(Integer EventId, Calendar Date) {
+            super(EventId, Date);
+        }
+
+        public Integer getEventId() {
+            return super.getKey();
+        }
+
+        public Calendar getTimeCalendar() {
+            return super.getValue();
+        }
+    }
+}
--- cool-core/src/main/java/com/nus/cool/core/cohort/refactor/TimeWindow.java
+++ cool-core/src/main/java/com/nus/cool/core/cohort/refactor/TimeWindow.java
@@ -1,13 +1,26 @@
 package com.nus.cool.core.cohort.refactor;
 
+import com.google.common.base.Preconditions;
 import com.nus.cool.core.cohort.refactor.TimeUtils.TimeUnit;
 
-public class TimeWindow {
-    public long length;
-    public TimeUnit unit;
+import lombok.Getter;
+
+public class TimeWindow implements Comparable<TimeWindow>{
+
+    @Getter
+    private long length;
+    @Getter
+    private TimeUnit unit;
 
     public TimeWindow(long length, TimeUnit unit){
         this.length = length;
         this.unit = unit;
+    }
+
+    @Override
+    public int compareTo(TimeWindow arg) {
+        Preconditions.checkArgument(arg.unit == this.unit, 
+            "Different TimeUnit isn't allowed to compare");
+        return Long.compare(this.length, arg.length);
     }
 }
