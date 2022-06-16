e6180403fbf4b8f572b069587e1fafa43d6878ea
Zrealshadow <704309740@qq.com>
2022-06-16 17:20:08 +0800

    extract sub-class RangeUnit as a standalone class

---
+++ cool-core/src/main/java/com/nus/cool/core/cohort/refactor/Scope.java
@@ -0,0 +1,14 @@
+package com.nus.cool.core.cohort.refactor;
+
+public class Scope {
+    private Integer left, right;
+
+    public Scope(Integer l, Integer r){
+        this.left = l == null ? Integer.MIN_VALUE: l; 
+        this.right = r == null ? Integer.MAX_VALUE : r;
+    }
+
+    public Boolean IsInScope(Integer i){
+        return i <= this.right && i >= this.left;
+    }
+}
