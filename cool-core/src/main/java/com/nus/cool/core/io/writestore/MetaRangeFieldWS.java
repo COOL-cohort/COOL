ba646147fe6837aeb616e9800433420af7fe63d6
liuchangshiye <liuchangshiye@gmail.com>
2022-06-20 14:23:13 +0800

    Changed write function for meta range field

--- cool-core/src/main/java/com/nus/cool/core/io/writestore/MetaRangeFieldWS.java
+++ cool-core/src/main/java/com/nus/cool/core/io/writestore/MetaRangeFieldWS.java
@@ -98,8 +98,18 @@ public class MetaRangeFieldWS implements MetaFieldWS {
   }
 
   @Override
+  public int writeUserTo(DataOutput out) throws IOException {
+    return 0;
+  }
+
+  @Override
   public void update(String v) {
     throw new UnsupportedOperationException("Doesn't support update now");
+  }
+
+  @Override
+  public void putUser(String[] tupleValue) {
+
   }
 
   @Override
