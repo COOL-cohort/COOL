1c3de9d0394914ea417b3ff77fa407ffc62ae027
Zrealshadow <704309740@qq.com>
2022-06-22 13:46:38 +0800

    change the interface | input a project tuple instead of only one object value

--- cool-core/src/main/java/com/nus/cool/core/cohort/refactor/cohortSelect/CohortSelector.java
+++ cool-core/src/main/java/com/nus/cool/core/cohort/refactor/cohortSelect/CohortSelector.java
@@ -1,7 +1,9 @@
 package com.nus.cool.core.cohort.refactor.cohortSelect;
 
+import com.nus.cool.core.cohort.refactor.storage.ProjectedTuple;
+
 public interface CohortSelector {
-    public String selectCohort(Object input);
+    public String selectCohort(ProjectedTuple tuple);
     
     public String getSchema();
 }
