8b696ebbbcd16e95100d038ae90d123b43136983
Zrealshadow <704309740@qq.com>
2022-06-25 20:55:10 +0800

    ValueSelection: add update RetUnit interface

--- cool-core/src/main/java/com/nus/cool/core/cohort/refactor/valueSelect/ValueSelection.java
+++ cool-core/src/main/java/com/nus/cool/core/cohort/refactor/valueSelect/ValueSelection.java
@@ -3,9 +3,13 @@ package com.nus.cool.core.cohort.refactor.valueSelect;
 import java.util.ArrayList;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.nus.cool.core.cohort.refactor.aggregate.AggregateFactory;
+import com.nus.cool.core.cohort.refactor.aggregate.AggregateFunc;
+import com.nus.cool.core.cohort.refactor.aggregate.AggregateType;
 import com.nus.cool.core.cohort.refactor.filter.Filter;
 import com.nus.cool.core.cohort.refactor.filter.FilterLayout;
 import com.nus.cool.core.cohort.refactor.storage.ProjectedTuple;
+import com.nus.cool.core.cohort.refactor.storage.RetUnit;
 
 import lombok.Getter;
 
@@ -25,6 +29,9 @@ public class ValueSelection {
     private ArrayList<String> schemaList;
     // private
 
+    @JsonIgnore
+    private AggregateFunc aggregateFunc;
+
     public void init() {
         this.filterList = new ArrayList<>();
         this.schemaList = new ArrayList<>();
@@ -34,8 +41,16 @@ public class ValueSelection {
         }
         this.filters = null;
         // reclaim the FilterLayout
+        // create func
+        this.aggregateFunc = AggregateFactory.generateAggregate(this.function);
     }
 
+    /**
+     * The filter will outline abnormal tuples
+     * 
+     * @param tuple
+     * @return
+     */
     public boolean IsSelected(ProjectedTuple tuple) {
         for (Filter filter : filterList) {
             switch (filter.getType()) {
@@ -51,6 +66,22 @@ public class ValueSelection {
             }
         }
         return true;
+    }
+
+    /**
+     * change the RetUnit according the aggregatefunc and modify it in place
+     * 
+     * @param retUnit
+     * @param tuple
+     */
+    public void updateRetUnit(RetUnit retUnit, ProjectedTuple tuple) {
+        if (this.aggregateFunc.getType() == AggregateType.COUNT) {
+            this.aggregateFunc.calulate(retUnit, 0);
+        }
+        // TODO(lingze)
+        // add validation step, this code is dangerous
+        int v = (int) (tuple.getValueBySchema(this.observedSchema));
+        this.aggregateFunc.calulate(retUnit, v);
     }
 
 }
