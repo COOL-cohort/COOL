7a1fdd2f516d6c6d3de782f9eaf3d08921392b8a
Zrealshadow <704309740@qq.com>
2022-06-25 20:55:36 +0800

    finish CohortProcessor main body

--- cool-core/src/main/java/com/nus/cool/core/cohort/refactor/CohortProcessor.java
+++ cool-core/src/main/java/com/nus/cool/core/cohort/refactor/CohortProcessor.java
@@ -3,6 +3,7 @@ package com.nus.cool.core.cohort.refactor;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -10,11 +11,15 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nus.cool.core.cohort.refactor.ageSelect.AgeSelection;
+import com.nus.cool.core.cohort.refactor.aggregate.AggregateFactory;
+import com.nus.cool.core.cohort.refactor.aggregate.AggregateFunc;
 import com.nus.cool.core.cohort.refactor.birthSelect.BirthSelection;
 import com.nus.cool.core.cohort.refactor.cohortSelect.CohortSelectionLayout;
 import com.nus.cool.core.cohort.refactor.cohortSelect.CohortSelector;
 import com.nus.cool.core.cohort.refactor.storage.CohortRet;
 import com.nus.cool.core.cohort.refactor.storage.ProjectedTuple;
+import com.nus.cool.core.cohort.refactor.storage.RetUnit;
+import com.nus.cool.core.cohort.refactor.utils.DateUtils;
 import com.nus.cool.core.cohort.refactor.valueSelect.ValueSelection;
 import com.nus.cool.core.io.readstore.ChunkRS;
 import com.nus.cool.core.io.readstore.CubeRS;
@@ -24,6 +29,9 @@ import com.nus.cool.core.io.readstore.HashMetaFieldRS;
 import com.nus.cool.core.io.readstore.MetaChunkRS;
 import com.nus.cool.core.io.readstore.MetaFieldRS;
 import com.nus.cool.core.schema.FieldType;
+
+import javafx.util.Pair;
+import lombok.Getter;
 
 public class CohortProcessor {
 
@@ -42,26 +50,30 @@ public class CohortProcessor {
     private List<String> projectedSchemaList;
 
     @JsonIgnore
-    private boolean inilialize =  false;
-    
+    private boolean inilialize = false;
+
     @JsonIgnore
     private final String UserIdSchema = "UserID";
-    
+
     @JsonIgnore
     private final String ActionTimeSchema = "ActionTime";
 
     @JsonIgnore
     private ProjectedTuple tuple;
 
+    @Getter
+    @JsonIgnore
+    private CohortRet result;
+
     /**
      * Create some filter instance
      */
-    public void init(){
+    public void init() {
         this.inilialize = true;
         this.ageSelector.init();
         this.cohortSelector = this.cohortSelectionLayout.generateCohortSelector();
         this.birthSelector.init();
-        // Merge Schema 
+        // Merge Schema
         HashSet<String> schemaMap = new HashSet<>();
         // Default Add UserIdSchema and ActionTimeSchema
         schemaMap.add(this.UserIdSchema);
@@ -71,15 +83,19 @@ public class CohortProcessor {
         schemaMap.addAll(this.birthSelector.getRelatedSchemas());
         this.projectedSchemaList = new ArrayList<>(schemaMap);
         this.tuple = new ProjectedTuple(this.projectedSchemaList);
+
+        // generate CohortResult to store intermediate result
+        this.result = new CohortRet(this.ageSelector);
     }
 
     /**
      * Read from json file and create a instance of CohortProcessor
+     * 
      * @param in
      * @return
      * @throws IOException
      */
-    public static CohortProcessor readFromJson(InputStream in) throws IOException{
+    public static CohortProcessor readFromJson(InputStream in) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         CohortProcessor instance = mapper.readValue(in, CohortProcessor.class);
         instance.init();
@@ -88,11 +104,12 @@ public class CohortProcessor {
 
     /**
      * Public interface, Scan whole table and return CohortResult
+     * 
      * @param cube
      * @return CohortRet
      */
-    public CohortRet process(CubeRS cube){
-        for(CubletRS cublet : cube.getCublets()){
+    public CohortRet process(CubeRS cube) {
+        for (CubletRS cublet : cube.getCublets()) {
             processCublet(cublet);
         }
         return null;
@@ -100,53 +117,97 @@ public class CohortProcessor {
 
     /**
      * Process one Cublet
+     * 
      * @param cublet
      */
-    private void processCublet(CubletRS cublet){
+    private void processCublet(CubletRS cublet) {
         MetaChunkRS metaChunk = cublet.getMetaChunk();
-        if(!this.checkMetaChunk(metaChunk)){return;}
+        if (!this.checkMetaChunk(metaChunk)) {
+            return;
+        }
         // if it is necessary, add logic in method checkMetaChunk
         // Personally, this step is not universal
         // all right, can check whether this cublet pass rangeFilter
 
-        // for Hash Value, Maintain HashMap<Schema, String[]>, the String[] is gid -> True Value
-        HashMap<String, String[]> gidMapBySchema =  new HashMap<>();
-        
+        // for Hash Value, Maintain HashMap<Schema, String[]>, the String[] is gid ->
+        // True Value
+        HashMap<String, String[]> gidMapBySchema = new HashMap<>();
+
         // we only get used schema;
-        for(String schema: this.projectedSchemaList){
+        for (String schema : this.projectedSchemaList) {
             MetaFieldRS metaField = metaChunk.getMetaField(schema);
-            if(IsHashType(metaField.getFieldType())){
-                gidMapBySchema.put(schema, ((HashMetaFieldRS)metaField).getGidMap());
+            if (FieldType.IsHashType(metaField.getFieldType())) {
+                gidMapBySchema.put(schema, ((HashMetaFieldRS) metaField).getGidMap());
             }
         }
 
         // Now start to pass the DataChunk
-        for(ChunkRS chunk: cublet.getDataChunks()){
-            this.processDataChunk(chunk,gidMapBySchema);
+        for (ChunkRS chunk : cublet.getDataChunks()) {
+            this.processDataChunk(chunk, gidMapBySchema);
         }
     }
 
-    private void processDataChunk(ChunkRS chunk, HashMap<String, String[]> hashMapperBySchema){
-        // maintain a collection fields
-        HashMap<String, FieldRS> fieldRSBySchema = new HashMap<>();
-        for(String schema:this.projectedSchemaList){
-            fieldRSBySchema.put(schema, chunk.getField(schema));
-        }
-
-        for(int i = 0; i < chunk.getRecords(); i++){
+    /**
+     * In this section, we load the tuple which is a inner property.
+     * We left the process logic in procesTuple function.
+     * 
+     * @param chunk
+     * @param hashMapperBySchema
+     */
+    private void processDataChunk(ChunkRS chunk, HashMap<String, String[]> hashMapperBySchema) {
+        for (int i = 0; i < chunk.getRecords(); i++) {
             // load data into tuple
-            for(String schema: this.projectedSchemaList){
-                // tuple.loadAttr(, schema);
+            for (String schema : this.projectedSchemaList) {
+                tuple.loadAttr(chunk.getField(schema).getValueByIndex(i), schema);
             }
+            // now this tuple is loaded
+            this.processTuple();
         }
     }
 
-    
-    private boolean IsHashType(FieldType fieldType) {
-        return false;
+    /**
+     * process the inner tuple
+     */
+    private void processTuple() {
+        // For One Tuple, we firstly get the userId, and ActionTime
+        int userId = (int) tuple.getValueBySchema(this.UserIdSchema);
+        Calendar actionTime = DateUtils.createCalender((long) tuple.getValueBySchema(this.ActionTimeSchema));
+        // check whether its birthEvent is selected
+        if (!this.birthSelector.isUserSelected(userId)) {
+            // if birthEvent is not selected
+            this.birthSelector.selectEvent(userId, actionTime, this.tuple);
+        } else {
+            // the birthEvent is selected
+
+            // do time_diff to generate age
+            // get the BirthEvent Date
+            Calendar birthTime = this.birthSelector.getUserBirthEventDate(userId);
+            int age = this.ageSelector.generateAge(birthTime, actionTime);
+            if (age == AgeSelection.DefaultNullAge) {
+                // age is outofrange
+                return;
+            }
+
+            // extract the cohort this tuple belong to
+            String cohortName = this.cohortSelector.selectCohort(this.tuple);
+            if (cohortName == null) {
+                // cohort is outofrange
+                return;
+            }
+
+            if (!this.valueSelector.IsSelected(this.tuple)) {
+                // value outofrange
+                return;
+            }
+            // Pass all above filter, we can store value into CohortRet
+            // get the temporay result for this CohortGroup and this age
+            RetUnit ret = this.result.get(cohortName, age);
+            // update
+            this.valueSelector.updateRetUnit(ret, tuple);
+        }
     }
 
-    private boolean checkMetaChunk(MetaChunkRS meta){
+    private boolean checkMetaChunk(MetaChunkRS meta) {
         return true;
     }
 }
