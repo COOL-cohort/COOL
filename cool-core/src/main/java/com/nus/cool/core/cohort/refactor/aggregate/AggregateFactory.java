83fb867891451ab22962892e43c04a9f6af99304
Zrealshadow <704309740@qq.com>
2022-06-27 21:54:31 +0800

    add JsonValue and JsonCreator annotation for objectMapper

--- cool-core/src/main/java/com/nus/cool/core/cohort/refactor/aggregate/AggregateFactory.java
+++ cool-core/src/main/java/com/nus/cool/core/cohort/refactor/aggregate/AggregateFactory.java
@@ -2,21 +2,22 @@ package com.nus.cool.core.cohort.refactor.aggregate;
 
 public class AggregateFactory {
 
-    public static AggregateFunc generateAggregate(String str) {
-        switch (str) {
-            case "Average":
-                new AverageFunc();
-            case "Count":
-                new CountFunc();
-            case "Max":
-                new MaxFunc();
-            case "Min":
-                new MinFunc();
-            case "Sum":
-                new SumFunc();
+    public static AggregateFunc generateAggregate(AggregateType type) {
+        switch (type) {
+            case AVERAGE:
+                return new AverageFunc();
+            case COUNT:
+                return new CountFunc();
+            case MAX:
+                return new MaxFunc();
+            case MIN:
+                return new MinFunc();
+            case SUM:
+                return new SumFunc();
             default:
                 throw new IllegalArgumentException(
-                        String.format("%s this type is not existed ", str));
+                        String.format("%s this type is not existed ", type.toString()));
+
         }
     }
 }
--- cool-core/src/main/java/com/nus/cool/core/cohort/refactor/aggregate/AggregateType.java
+++ cool-core/src/main/java/com/nus/cool/core/cohort/refactor/aggregate/AggregateType.java
@@ -1,18 +1,35 @@
 package com.nus.cool.core.cohort.refactor.aggregate;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum AggregateType {
-    AVERAGE("Average"), COUNT("Count"),
-    MAX("Max"), MIN("Min"), SUM("Sum");
+    AVERAGE("AVERAGE"), COUNT("COUNT"),
+    MAX("MAX"), MIN("MIN"), SUM("SUM");
 
     private final String text;
 
-    AggregateType(final String text) {
+    private AggregateType(final String text) {
         this.text = text;
     }
 
+    @JsonValue
     @Override
     public String toString() {
         return text;
+    }
+
+    @JsonCreator
+    public static AggregateType forValue(String str){
+        switch(str){
+            case "AVERAGE": return AVERAGE;
+            case "COUNT": return COUNT;
+            case "MAX": return MAX;
+            case "MIN": return MIN;
+            case "SUM": return SUM;
+            default:
+                throw new IllegalArgumentException();
+        }
     }
 
 }
