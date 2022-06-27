dd790980a3d2e9eb31314831ab5ec093aac0f9db
Zrealshadow <704309740@qq.com>
2022-06-27 22:05:26 +0800

    add JsonValue and JsonCreator annotation, add Getter annotation, add JSON_INCLUE_NOT_NULL for certain properties

--- cool-core/src/main/java/com/nus/cool/core/cohort/refactor/filter/FilterLayout.java
+++ cool-core/src/main/java/com/nus/cool/core/cohort/refactor/filter/FilterLayout.java
@@ -1,40 +1,47 @@
 package com.nus.cool.core.cohort.refactor.filter;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.google.common.base.Preconditions;
+
 import lombok.Getter;
 
-/** Read from json file into FilterLayout 
- *  Parse to construct certain filter class
+/**
+ * Read from json file into FilterLayout
+ * Parse to construct certain filter class
  **/
 public class FilterLayout {
     @Getter
     private String fieldSchema;
-    
+
     @Getter
-    private String type;
-    
+    private FilterType type;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     @Getter
     private String[] acceptValue;
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     @Getter
     private String[] rejectValue;
 
     @Override
     public String toString() {
-        return String.format("FilterLayout fieldSchema %s filter type %s, acceptValue %s, rejectValue %s", 
-            fieldSchema, type, acceptValue.toString(), rejectValue.toString());
+        return String.format("FilterLayout fieldSchema %s filter type %s, acceptValue %s, rejectValue %s",
+                fieldSchema, type, acceptValue.toString(), rejectValue.toString());
     }
 
+    public Filter generateFilter() {
+        switch (type) {
+            case Range:
+                Preconditions.checkArgument(rejectValue == null,
+                        "For RangeFilter, rejectValue property is null");
+                return new RangeFilter(fieldSchema, acceptValue);
+            case Set:
+                return new SetFilter(fieldSchema, acceptValue, rejectValue);
+            default:
+                throw new IllegalArgumentException(
+                        String.format("No filter of this type named %s", type));
+        }
 
-    public Filter generateFilter(){
-        if(type == FilterType.Range.toString()) {
-            // For RangeFilter, rejectValue property is null 
-            Preconditions.checkArgument(rejectValue == null, 
-                "For RangeFilter, rejectValue property is null");
-            return new RangeFilter(fieldSchema, acceptValue);
-        } else if (type == FilterType.Set.toString()){
-            return new SetFilter(fieldSchema,acceptValue, rejectValue);
-        } else throw new IllegalArgumentException(
-                String.format("No filter of this type named %s", type));   
     }
 }
