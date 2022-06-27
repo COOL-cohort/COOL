23e7d9b9c4861f372b7c20d385b62e52340640ee
Zrealshadow <704309740@qq.com>
2022-06-27 22:06:34 +0800

    add a default constructor for timewindows and add JsonCreator and JsonValue annotation for TimeUnit

--- cool-core/src/main/java/com/nus/cool/core/cohort/refactor/utils/TimeUtils.java
+++ cool-core/src/main/java/com/nus/cool/core/cohort/refactor/utils/TimeUtils.java
@@ -1,5 +1,8 @@
 package com.nus.cool.core.cohort.refactor.utils;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public class TimeUtils {
 
     public enum TimeUnit {
@@ -21,14 +24,33 @@ public class TimeUtils {
         
 
         private final String text;
-        TimeUnit(final String text) {
+        private TimeUnit(final String text) {
             this.text = text;
         }
 
+        @JsonValue
         @Override
         public String toString(){
             return text;
         }
+
+        @JsonCreator
+        public static TimeUnit forValue(String value){
+            switch(value){
+                case "HOUR" : return TimeUnit.HOUR;
+                case "DAY":return TimeUnit.DAY;
+                case "WEEK": return TimeUnit.WEEK;
+                case "MINUTE": return TimeUnit.MINUTE;
+                case "SECOND": return TimeUnit.SECOND;
+                case "YEAR": return TimeUnit.YEAR;
+                default:
+                    throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    public static TimeUnit GenerateTimeUnit(String str){
+        return TimeUnit.forValue(str);
     }
     
 }
