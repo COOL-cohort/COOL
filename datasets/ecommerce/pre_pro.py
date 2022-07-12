ff6b8ae43851a5a69542c6843525d6e1bf42fad4
NLGithubWP <xingnaili14@gmail.com>
2022-07-12 14:13:10 +0800

    Add comment to pre-processing script

--- datasets/ecommerce/pre_pro.py
+++ datasets/ecommerce/pre_pro.py
@@ -4,9 +4,7 @@
 
 import pandas as pd
 
-df = pd.read_csv('/Users/kevin/project_java/COOL/datasets/ecommerce/update-sample.csv')
-
-
-# df.sort_values("Reported_Date", inplace=True)
-
-# df.to_csv('/Users/kevin/project_java/COOL/datasets/ecommerce/update-sample.csv', index=False)
+# this will order the update-sample_ori.csv by Reported_date and then write back to another file
+df = pd.read_csv('/COOL/datasets/ecommerce/update-sample_ori.csv.csv')
+df.sort_values("Reported_Date", inplace=True)
+df.to_csv('/COOL/datasets/ecommerce/update-sample.csv', index=False)
