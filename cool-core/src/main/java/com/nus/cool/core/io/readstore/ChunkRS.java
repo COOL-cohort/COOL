59633cab9dc2c9026e4f4846a2b2ed3a56fcb53b
liuchangshiye <liuchangshiye@gmail.com>
2022-07-21 15:14:05 +0800

    change the logic for datachunk to deal with invariant data

--- cool-core/src/main/java/com/nus/cool/core/io/readstore/ChunkRS.java
+++ cool-core/src/main/java/com/nus/cool/core/io/readstore/ChunkRS.java
@@ -92,12 +92,16 @@ public class ChunkRS implements Input {
     // fieldOffsets="+ Arrays.toString(fieldOffsets));
 
     this.fields = new FieldRS[fields];
-    for (int i = 0; i < fields; i++) {
+    int fieldIndex=0;
+    for (int i = 0; i < schema.getFields().size(); i++) {
       // System.out.println("Reading data chunk's field ="+i+".....");
-      buffer.position(fieldOffsets[i]);
+      if(schema.getInvariantFields().contains(i)){
+        continue;
+      }
+      buffer.position(fieldOffsets[fieldIndex]);
       FieldRS field = new CoolFieldRS();
       field.readFromWithFieldType(buffer, this.schema.getField(i).getFieldType());
-      this.fields[i] = field;
+      this.fields[fieldIndex++] = field;
     }
   }
 
@@ -106,7 +110,7 @@ public class ChunkRS implements Input {
   }
 
   public FieldRS getField(String fieldName) {
-    return getField(schema.getFieldID(fieldName));
+    return getField(schema.getDataChunkFieldName2Id().get(fieldName));
   }
 
 }
