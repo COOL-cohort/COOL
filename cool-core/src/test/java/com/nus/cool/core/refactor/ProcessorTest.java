d01adaa7eaaafff976b35f3772d1d7552aa37d40
Zrealshadow <704309740@qq.com>
2022-08-21 13:49:35 +0800

    format

--- cool-core/src/test/java/com/nus/cool/core/refactor/ProcessorTest.java
+++ cool-core/src/test/java/com/nus/cool/core/refactor/ProcessorTest.java
@@ -56,8 +56,11 @@ public class ProcessorTest {
 
         String queryResultPath = Paths.get(queryDir, this.resultName).toString();
         ObjectMapper mapper = new ObjectMapper();
-        // HashMap<String, List<Integer>> cohortData = mapper.readValue(new File(queryResultPath), HashMap.class);
-        HashMap<String, List<Integer>> cohortData = mapper.readValue(new File(queryResultPath), new TypeReference<HashMap<String,List<Integer>>>(){});
+        // HashMap<String, List<Integer>> cohortData = mapper.readValue(new
+        // File(queryResultPath), HashMap.class);
+        HashMap<String, List<Integer>> cohortData = mapper.readValue(new File(queryResultPath),
+                new TypeReference<HashMap<String, List<Integer>>>() {
+                });
         // check the result
         System.out.println(ret.toString());
         // validate the cohortName
@@ -70,7 +73,7 @@ public class ProcessorTest {
             // System.out.printf("True Result %s\n", cohortData.get(cohortName).toString());
             // System.out.printf("Get Result %s\n", ret.getValuesByCohort(cohortName));
             Assert.assertEquals(cohortData.get(cohortName), ret.getValuesByCohort(cohortName));
-        
+
         }
     }
 
