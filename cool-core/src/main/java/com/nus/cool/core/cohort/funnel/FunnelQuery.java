c00b56aa448d2f426762c1d4b00ba4f76d530a24
cchrewrite <chenghao.cai@outlook.com>
2022-09-09 04:39:11 -0400

    Update FunnelQuery.java
    Remove redundant functions.

--- cool-core/src/main/java/com/nus/cool/core/cohort/funnel/FunnelQuery.java
+++ cool-core/src/main/java/com/nus/cool/core/cohort/funnel/FunnelQuery.java
@@ -21,40 +21,36 @@ public class FunnelQuery {
 		return stages;
 	}
 
-	public void setStages(List<BirthSequence> stages) {
-		this.stages = stages;
-	}
+	// public void setStages(List<BirthSequence> stages) {
+	//	 this.stages = stages;
+	// }
 
 	public boolean isOrdered() {
 		return isOrdered;
 	}
 
-	public void setOrdered(boolean isOrdered) {
-		this.isOrdered = isOrdered;
-	}
+	// public void setOrdered(boolean isOrdered) {
+	// 	 this.isOrdered = isOrdered;
+	// }
 
 	public String getDataSource() {
 		return dataSource;
 	}
 
-	public void setDataSource(String dataSource) {
-		this.dataSource = dataSource;
-	}
+	// public void setDataSource(String dataSource) {
+	//   this.dataSource = dataSource;
+	// }
 
 	public String getInputCohort() {
 		return inputCohort;
 	}
 
-	public void setInputCohort(String inputCohort) {
-		this.inputCohort = inputCohort;
-	}
+	// public void setInputCohort(String inputCohort) {
+	//	 this.inputCohort = inputCohort;
+	// }
 
 	@JsonIgnore
-	public boolean isValid() throws IOException {
-		if ((stages != null) && (dataSource != null)){
-			return true;
-		} else{
-			throw new IOException("[x] Invalid funnel query.");
-		}
+	public boolean isValid() {
+		return (stages != null) && (dataSource != null);
 	}
 }
