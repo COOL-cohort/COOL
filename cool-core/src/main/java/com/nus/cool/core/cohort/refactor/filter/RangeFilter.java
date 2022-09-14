1ba852c2f7e5b7be2400c995484350c915bf57fb
Zrealshadow <704309740@qq.com>
2022-09-14 22:26:17 +0800

    add loadMetaInfo interface for SetFilter

--- cool-core/src/main/java/com/nus/cool/core/cohort/refactor/filter/RangeFilter.java
+++ cool-core/src/main/java/com/nus/cool/core/cohort/refactor/filter/RangeFilter.java
@@ -1,127 +1,131 @@
 package com.nus.cool.core.cohort.refactor.filter;
 
-import com.google.common.base.Preconditions;
-import com.nus.cool.core.cohort.refactor.storage.Scope;
+import java.util.List;
 import java.util.ArrayList;
 import java.util.BitSet;
-import java.util.List;
+
+import com.google.common.base.Preconditions;
+import com.nus.cool.core.cohort.refactor.storage.Scope;
+import com.nus.cool.core.io.readstore.MetaChunkRS;
+
 import lombok.Getter;
 
 /**
- * Range filter class.
+ * Range filter class
  */
 public class RangeFilter implements Filter {
 
-  // Some static defined parameter
-  private static final FilterType type = FilterType.Range;
+    // Some static defined parameter
+    private static final FilterType type = FilterType.Range;
+    private static final String MinLimit = "MIN";
+    private static final String MaxLimit = "MAX";
+    @Getter
+    private static final String splitChar = "-";
 
-  private static final String MinLimit = "MIN";
-  
-  private static final String MaxLimit = "MAX";
-  
-  @Getter
-  private static final String splitChar = "-";
+    // accepted range
+    @Getter
+    private final List<Scope> acceptRangeList;
 
-  // accepted range
-  @Getter
-  private final List<Scope> acceptRangeList;
+    // filter schema
+    private final String fieldSchema;
 
-  // filter schema
-  private final String fieldSchema;
-
-  /**
-   * Create a range filter.
-   */
-  public RangeFilter(String fieldSchema, String[] acceptValues) {
-    this.fieldSchema = fieldSchema;
-    this.acceptRangeList = new ArrayList<Scope>();
-    for (String acceptValue : acceptValues) {
-      acceptRangeList.add(RangeFilter.parse(acceptValue));
+    public RangeFilter(String fieldSchema, String[] acceptValues) {
+        this.fieldSchema = fieldSchema;
+        this.acceptRangeList = new ArrayList<Scope>();
+        for (String acceptValue : acceptValues) {
+            acceptRangeList.add(RangeFilter.parse(acceptValue));
+        }
     }
-  }
 
-  /**
-   * Need add Scope in the next steps.
-
-   * @param fieldSchema String
-   */
-  public RangeFilter(String fieldSchema, List<Scope> scopeList) {
-    this.fieldSchema = fieldSchema;
-    this.acceptRangeList = scopeList;
-  }
-
-  @Override
-  public Boolean accept(String value) throws RuntimeException {
-    throw new UnsupportedOperationException(
-        "RangeFilter dosent't implement the Integer accept method");
-  }
-
-  @Override
-  public Boolean accept(Integer value) throws RuntimeException {
-    for (Scope u : acceptRangeList) {
-      if (u.isInScope(value)) {
-        return true;
-      }
+    /**
+     * Need add Scope in the next steps
+     * 
+     * @param fieldSchema String
+     */
+    public RangeFilter(String fieldSchema, List<Scope> scopeList) {
+        this.fieldSchema = fieldSchema;
+        this.acceptRangeList = scopeList;
     }
-    return false;
-  }
 
-  @Override
-  public BitSet accept(String[] values) throws RuntimeException {
-    throw new UnsupportedOperationException(
-        "RangeFilter dosent't implement the Integer accept method");
-  }
-
-  @Override
-  public BitSet accept(List<Integer> values) throws RuntimeException {
-    BitSet res = new BitSet(values.size());
-    for (int i = 0; i < values.size(); i++) {
-      if (accept(values.get(i))) {
-        res.set(i);
-      }
+    @Override
+    public Boolean accept(Integer value) throws RuntimeException {
+        for (Scope u : acceptRangeList) {
+            if (u.IsInScope(value)) {
+                return true;
+            }
+        }
+        return false;
     }
-    return res;
-  }
 
-  @Override
-  public BitSet accept(Scope scope) throws RuntimeException {
-    BitSet res = new BitSet(1);
-    for (Scope u : acceptRangeList) {
-      if (u.isIntersection(scope)) {
-        res.set(1);
-      }
+    @Override
+    public BitSet accept(List<Integer> values) throws RuntimeException {
+        BitSet res = new BitSet(values.size());
+        for (int i = 0; i < values.size(); i++) {
+            if (accept(values.get(i))) {
+                res.set(i);
+            }
+        }
+        return res;
     }
-    return res;
-  }
 
-  @Override
-  public FilterType getType() {
-    return type;
-  }
-
-  /**
-   * Parse string to RangeUnit Exmaple [145 - 199] = RangeUnit{left:145 ,
-   * right:199}.
-
-   * @param str string
-   * @return RangeUnit
-   */
-  private static Scope parse(String str) {
-    String[] part = str.split(splitChar);
-    Preconditions.checkArgument(part.length == 2, "Split RangeUnit failed");
-    Integer l = null;
-    Integer r = null;
-    if (!part[0].equals(MinLimit)) {
-      l = Integer.parseInt(part[0]);
+    @Override
+    public boolean accept(Scope scope) throws RuntimeException {
+        for (Scope u : acceptRangeList) {
+            if (u.IsSubset(scope)) {
+                return true;
+            }
+        }
+        return false;
     }
-    if (!part[1].equals(MaxLimit)) {
-      r = Integer.parseInt(part[1]);
-    }
-    return new Scope(l, r);
-  }
 
-  @Override
-  public String getFilterSchema() {
-    return this.fieldSchema;
-  }
+    @Override
+    public FilterType getType() {
+        return type;
+    }
+
+    /**
+     * Parse string to RangeUnit
+     * Exmaple [145 - 199] = RangeUnit{left:145 , right:199}
+     * 
+     * @param str string
+     * @return RangeUnit
+     */
+    private static Scope parse(String str) {
+        String[] part = str.split(splitChar);
+        Preconditions.checkArgument(part.length == 2,
+                "Split RangeUnit failed");
+        Integer l = null, r = null;
+        if (!part[0].equals(MinLimit)) {
+            l = Integer.parseInt(part[0]);
+        }
+        if (!part[1].equals(MaxLimit)) {
+            r = Integer.parseInt(part[1]);
+        }
+        return new Scope(l, r);
+    }
+
+    @Override
+    public String getFilterSchema() {
+        return this.fieldSchema;
+    }
+
+    @Override
+    public void loadMetaInfo(MetaChunkRS metaChunkRS) {
+        // for range Filter, no need to load info 
+    }
+
+
+    // ---------------  compatable with old version -----------------
+
+    public Boolean accept(String value) throws RuntimeException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public BitSet accept(String[] values) throws RuntimeException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+   
 }
