a47daf2111aae09b92fe471ed64a1aaa4dae4b7e
liuchangshiye <liuchangshiye@gmail.com>
2022-09-14 13:43:39 +0800

    add unit test for field sequence consistency check

--- cool-core/src/main/java/com/nus/cool/model/CoolLoader.java
+++ cool-core/src/main/java/com/nus/cool/model/CoolLoader.java
@@ -96,8 +96,8 @@ public class CoolLoader {
         if(dataFileName.endsWith(".csv")){
             boolean bconsistency =DataLoader.checkConsistency(schema.getFields(), ((CsvDataLoaderConfig)this.loaderConfig).getDataFieldName());
             if(!bconsistency){
-                logger.error("The field sequence of the table.YAML must be the same as that of data file!");
-                System.exit(-1);
+                logger.error("The field sequence of the data file must be the same as that of the table.YAML!");
+                throw new IOException("Field Sequence");
             }
         }
         loader.load();
