88d9d418b6361a68f1bb8001d14a922ebc73bf52
Zrealshadow <704309740@qq.com>
2022-10-11 16:30:34 +0800

    split the rejectHashFilter and acceptHashFilter

---
+++ cool-core/src/main/java/com/nus/cool/core/cohort/refactor/filter/SetRejectFilter.java
@@ -0,0 +1,18 @@
+package com.nus.cool.core.cohort.refactor.filter;
+
+public class SetRejectFilter extends SetFilter {
+
+
+    public SetRejectFilter(String fieldSchema, String[] rejectValues) {
+        super(fieldSchema, rejectValues);
+    }
+
+    @Override
+    public Boolean accept(Integer value) throws RuntimeException {
+        if (this.gidSet == null)
+            throw new RuntimeException("Filter is not initialized");
+        return !this.gidSet.contains(value);
+        // return this.
+    }
+
+}
