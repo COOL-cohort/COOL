f5bfd15892e1a62b19735385587624ef39232b67
Zrealshadow <704309740@qq.com>
2022-10-27 15:50:10 +0800

    rename some dir

--- cool-core/src/main/java/com/nus/cool/core/cohort/refactor/birthselect/BirthSelection.java
+++ cool-core/src/main/java/com/nus/cool/core/cohort/refactor/birthselect/BirthSelection.java
@@ -1,86 +1,113 @@
-package com.nus.cool.core.cohort.refactor.birthselect;
+package com.nus.cool.core.cohort.refactor.birthSelect;
 
-import com.nus.cool.core.cohort.refactor.storage.ProjectedTuple;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+
+import com.nus.cool.core.cohort.BirthSequence.BirthEvent;
+import com.nus.cool.core.cohort.refactor.filter.Filter;
+import com.nus.cool.core.cohort.refactor.storage.ProjectedTuple;
+import com.nus.cool.core.io.readstore.MetaChunkRS;
+
 import lombok.Getter;
 
 /**
- * This class shows how to choose the birthAction according to cohort query.
- */
+ * This class shows how to choose the birthAction according to cohort query
+ */ 
 @Getter
 public class BirthSelection {
 
-  // a list of events filter, if any item pass the filter, it can be considered as
-  // selected event.
-  // if the birthEvents is null, we thought that any action condition can be
-  // regareded as birthAction
-  private List<EventSelection> birthEvents;
+    // a list of events filter, if any item pass the filter, it can be considered as selected event. 
+    // if the birthEvents is null, we thought that any action condition can be regareded as birthAction
+    private List<EventSelection> birthEvents;
+    
+    private BirthSelectionContext context;
 
-  private BirthSelectionContext context;
 
-  /**
-   * Create BirthSelection from BirthSelectionLayout.
-   */
-  public BirthSelection(List<EventSelection> events, BirthSelectionContext context) {
-    this.birthEvents = events;
-    this.context = context;
-  }
-
-  /**
-   * If user's birthEvent is selected return true else return false.
-
-   * @return whether the user's birthEvent is selected
-   */
-  public boolean isUserSelected(String userId) {
-    return context.isUserSelected(userId);
-  }
-
-  /**
-   * If user's birthEvent is selected, Get the birthEvent's Date. If cohort query
-   * requires a collection of event we take the first event in whole collection as
-   * user's birthEvent
-   */
-  public LocalDateTime getUserBirthEventDate(String userId) {
-    return context.getUserBirthEventDate(userId);
-  }
-
-  /**
-   * Select input Action Tuple, if it can not be selected as event return false
-   * Else return true and add this event into BirthSelectionContext for further
-   * check.
-
-   * @param tuple  Partial Action Tuple,
-   */
-  public boolean selectEvent(String userId, LocalDateTime date, ProjectedTuple tuple) {
-    int eventIdx = 0;
-    // if no birthEvents in query
-    // which means that all event is acceptable
-    // thus, we just to record the first actionItem's time.
-    if (birthEvents == null) {
-      if (!context.isUserSelected(userId)) {
-        context.setUserSelected(userId, date);
-        return true;
-      }
-      return false;
+    /**
+     * Create BirthSelection from BirthSelectionLayout
+     * @param events
+     * @param Context
+     */
+    public BirthSelection(List<EventSelection> events, BirthSelectionContext Context){
+        this.birthEvents =  events;
+        this.context = Context;
     }
 
-    // if birthEvents is not null
-    for (EventSelection event : birthEvents) {
-      if (event.accept(tuple)) {
-        context.add(userId, eventIdx, date);
-        return true;
-      }
-      eventIdx++;
+    /**
+     * If user's birthEvent is selected return true else return false
+     * @param userId 
+     * @return whether the user's birthEvent is selected
+     */
+    public boolean isUserSelected(String userId) {
+        return context.isUserSelected(userId);
     }
-    return false;
-  }
 
-  /**
-   * Returns the list of userId of users having eligiable birthEvent.
-   */
-  public Set<String> getAcceptedUsers() {
-    return this.context.getSelectedUserId();
-  }
+    /**
+     * If user's birthEvent is selected, Get the birthEvent's Date.
+     * If cohort query requires a collection of event
+     * we take the first event in whole collection as user's birthEvent
+     * 
+     * @param userId
+     * @return
+     */
+    public LocalDateTime getUserBirthEventDate(String userId) {
+        return context.getUserBirthEventDate(userId);
+    }
+
+    /**
+     * Select input Action Tuple, if it can not be selected as event return false
+     * Else return true and add this event into BirthSelectionContext for further check.
+     * @param userId
+     * @param date
+     * @param tuple  Partial Action Tuple,
+     * @return
+     */
+    public boolean selectEvent(String userId, LocalDateTime date, ProjectedTuple tuple) {
+        int eventIdx = 0;
+        // if no birthEvents in query
+        // which means that all event is acceptable
+        // thus, we just to record the first actionItem's time.
+        if(birthEvents == null){
+            if(!context.isUserSelected(userId)){
+                context.setUserSelected(userId, date);
+                return true;
+            }
+            return false;
+        }
+
+        // if birthEvents is not null
+        for (EventSelection event : birthEvents) {
+            if (event.Accept(tuple)) {
+                context.Add(userId, eventIdx, date);
+                return true;
+            }
+            eventIdx++;
+        }
+        return false;
+    }
+
+    /** 
+     * @return a list of userId, in which any user have eligiable birthEvent
+     */
+    public Set<String> getAcceptedUsers(){
+        return this.context.getSelectedUserId();
+    }
+
+    /**
+     * Before processing, change the value in setFilter into globalID
+     * @param metaChunkRS
+     */
+    public List<Filter> getFilterList(){
+        List<Filter> ret = new ArrayList<Filter>();
+        if(this.birthEvents == null) {
+            return ret;
+        }
+
+        for(EventSelection eventSelection:this.birthEvents){
+            ret.addAll(eventSelection.getFilterList());
+        }
+        return ret;
+    }
 }
--- cool-core/src/main/java/com/nus/cool/core/cohort/refactor/cohortselect/CohortSetSelector.java
+++ cool-core/src/main/java/com/nus/cool/core/cohort/refactor/cohortselect/CohortSetSelector.java
@@ -1,44 +1,46 @@
-package com.nus.cool.core.cohort.refactor.cohortselect;
-
+package com.nus.cool.core.cohort.refactor.cohortSelect;
 import com.nus.cool.core.cohort.refactor.filter.Filter;
 import com.nus.cool.core.cohort.refactor.filter.SetFilter;
 import com.nus.cool.core.cohort.refactor.storage.ProjectedTuple;
+import com.nus.cool.core.io.readstore.MetaChunkRS;
+import com.nus.cool.core.io.readstore.MetaFieldRS;
+
 
 /**
- * Class CohortSetSelector for set type column schema. It helps to judge whether
- * the value in cohortSchema is acceptable
+ * Class CohortSetSelector for set type column schema
+ * It helps to judge whether the value in cohortSchema is acceptable
  */
 public class CohortSetSelector implements CohortSelector {
 
-  private final SetFilter filter;
-
-  public CohortSetSelector(String fieldSchema, String[] acceptValues, String[] rejectedValues) {
-    this.filter = new SetFilter(fieldSchema, acceptValues, rejectedValues);
-  }
-
-  /**
-   * Select the cohort based on the set field value.
-   */
-  public String selectCohort(Object input) {
-    String s = String.valueOf(input);
-    if (this.filter.accept(s)) {
-      return s;
+    private final Filter filter;
+    
+    public CohortSetSelector(String fieldSchema, String[] acceptValues, String[] rejectedValues) {
+        this.filter = SetFilter.GenerateSetFilter(fieldSchema, acceptValues, rejectedValues);
     }
-    return null;
-  }
 
-  @Override
-  public String selectCohort(ProjectedTuple tuple) {
-    return selectCohort(tuple.getValueBySchema(this.getSchema()));
-  }
+    private Integer selectCohort(Object input) {
+        Integer i = (Integer)input;
+        if(this.filter.accept(i)) return i;
+        return null;
+    }
 
-  @Override
-  public String getSchema() {
-    return this.filter.getFilterSchema();
-  }
+    public String getSchema(){
+        return this.filter.getFilterSchema();
+    }
 
-  @Override
-  public Filter getFilter() {
-    return filter;
-  }
+    @Override
+    public String selectCohort(ProjectedTuple tuple, MetaChunkRS metaChunkRS) {
+        Integer gid = selectCohort(tuple.getValueBySchema(this.getSchema()));
+        if(gid == null) {
+            throw new RuntimeException("GlobalID of Selected Cohort Should Exist in Cublet");
+        }
+        MetaFieldRS metaField = metaChunkRS.getMetaField(this.filter.getFilterSchema());
+        return metaField.getString(gid);
+    }
+
+    @Override
+    public Filter getFilter(){
+        return filter;
+    }
+    
 }
