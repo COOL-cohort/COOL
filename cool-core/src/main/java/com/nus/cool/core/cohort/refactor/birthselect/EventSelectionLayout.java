648cd371271c35a631336bc2d30d89c7e884859d
Zrealshadow <704309740@qq.com>
2022-10-27 15:50:10 +0800

    rename some dir

--- cool-core/src/main/java/com/nus/cool/core/cohort/refactor/birthselect/EventSelectionLayout.java
+++ cool-core/src/main/java/com/nus/cool/core/cohort/refactor/birthselect/EventSelectionLayout.java
@@ -1,50 +1,42 @@
-package com.nus.cool.core.cohort.refactor.birthselect;
+package com.nus.cool.core.cohort.refactor.birthSelect;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.nus.cool.core.cohort.refactor.filter.Filter;
 import com.nus.cool.core.cohort.refactor.filter.FilterLayout;
-import java.util.ArrayList;
-import java.util.List;
+
 import lombok.Getter;
 
-/**
- * Layout class to facilitate json serialization of event selection criteria.
- */
+
 public class EventSelectionLayout {
-  @Getter
-  @JsonProperty("filters")
-  private List<FilterLayout> filters;
+    @Getter
+    @JsonProperty("filters")
+    private List<FilterLayout> filters;
 
-  @JsonIgnore
-  private List<String> schemaList;
+    @JsonIgnore
+    private List<String> schemaList;
 
-  @Getter
-  @JsonProperty("frequency")
-  private int frequency;
+    @Getter
+    @JsonProperty("frequency")
+    private int frequency;
 
-  /**
-   * Deserialize EventSelection.
-   */
-  public EventSelection generate() {
-    ArrayList<Filter> filterList = new ArrayList<>();
-    for (FilterLayout layout : filters) {
-      filterList.add(layout.generateFilter());
+    public EventSelection generate(){
+        ArrayList<Filter> filterList = new ArrayList<>();
+        for(FilterLayout layout : filters){
+            filterList.add(layout.generateFilter());
+        }
+        return new EventSelection(filterList);
+    };
+
+    public List<String> getSchemaList(){
+        if(this.schemaList != null) return this.schemaList; 
+        this.schemaList =  new ArrayList<>();
+        for(FilterLayout layout: filters){
+            this.schemaList.add(layout.getFieldSchema());
+        }
+        return this.schemaList;
     }
-    return new EventSelection(filterList);
-  }
-
-  /**
-   * Return schema in a list.
-   */
-  public List<String> getSchemaList() {
-    if (this.schemaList != null) {
-      return this.schemaList;
-    }
-    this.schemaList = new ArrayList<>();
-    for (FilterLayout layout : filters) {
-      this.schemaList.add(layout.getFieldSchema());
-    }
-    return this.schemaList;
-  }
 }
--- cool-core/src/main/java/com/nus/cool/core/cohort/refactor/valueselect/ValueSelectionLayout.java
+++ cool-core/src/main/java/com/nus/cool/core/cohort/refactor/valueselect/ValueSelectionLayout.java
@@ -1,4 +1,8 @@
-package com.nus.cool.core.cohort.refactor.valueselect;
+package com.nus.cool.core.cohort.refactor.valueSelect;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -8,58 +12,45 @@ import com.nus.cool.core.cohort.refactor.aggregate.AggregateFunc;
 import com.nus.cool.core.cohort.refactor.aggregate.AggregateType;
 import com.nus.cool.core.cohort.refactor.filter.Filter;
 import com.nus.cool.core.cohort.refactor.filter.FilterLayout;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+
 import lombok.Getter;
 
-/**
- * Layout class to facilitate json serialization of value selection criteria.
- */
 @Getter
 public class ValueSelectionLayout {
 
-  @JsonInclude(JsonInclude.Include.NON_NULL)
-  private List<FilterLayout> filters;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private List<FilterLayout> filters;
 
-  @JsonProperty("function")
-  private AggregateType functionType;
+    @JsonProperty("function")
+    private AggregateType functionType;
 
-  @JsonProperty("observedSchema")
-  private String chosenSchema;
+    @JsonProperty("observedSchema")
+    private String chosenSchema;
 
-  @JsonIgnore
-  private HashSet<String> schemaList;
+    @JsonIgnore
+    private HashSet<String> schemaList;
 
-  /**
-   * Deserialize ValueSelection.
-   */
-  public ValueSelection generate() {
-    List<Filter> filterList = null;
-    if (this.filters != null) {
-      filterList = new ArrayList<>();
-      for (FilterLayout layout : this.filters) {
-        filterList.add(layout.generateFilter());
-      }
+    public ValueSelection generate(){
+        List<Filter> filterList = null;
+        if(this.filters != null) {
+            filterList = new ArrayList<>();
+            for(FilterLayout layout : this.filters){
+                filterList.add(layout.generateFilter());
+            }
+        }
+        AggregateFunc func =  AggregateFactory.generateAggregate(functionType, chosenSchema);
+        return new ValueSelection(filterList, func);
     }
-    AggregateFunc func = AggregateFactory.generateAggregate(functionType, chosenSchema);
-    return new ValueSelection(filterList, func);
-  }
 
-  /**
-   * Return the schema list in a list of string.
-   */
-  public List<String> getSchemaList() {
-    if (schemaList == null) {
-      schemaList = new HashSet<>();
-      if (this.filters == null) {
+    public List<String> getSchemaList(){
+        if(schemaList == null){
+            schemaList = new HashSet<>();
+            if(this.filters == null) return new ArrayList<>(schemaList);
+            for(FilterLayout layout: this.filters){
+                schemaList.add(layout.getFieldSchema());
+            }
+        }
         return new ArrayList<>(schemaList);
-      }
-      for (FilterLayout layout : this.filters) {
-        schemaList.add(layout.getFieldSchema());
-      }
     }
-    return new ArrayList<>(schemaList);
-  }
-
+    
 }
