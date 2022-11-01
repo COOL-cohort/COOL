5ac5d371f0a2bd3bfd9b4f95e84f2c6c7ee483ae
Zrealshadow <704309740@qq.com>
2022-11-01 19:47:37 +0800

    add test dependency

--- cool-core/src/test/java/com/nus/cool/core/iceberg/query/IcebergQueryTest.java
+++ cool-core/src/test/java/com/nus/cool/core/iceberg/query/IcebergQueryTest.java
@@ -1,4 +1,5 @@
 package com.nus.cool.core.iceberg.query;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nus.cool.core.cohort.QueryResult;
 import com.nus.cool.core.iceberg.aggregator.AggregatorFactory;
@@ -16,7 +17,6 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-
 public class IcebergQueryTest {
 
     IcebergQuery query;
@@ -24,11 +24,11 @@ public class IcebergQueryTest {
     @BeforeMethod
     public void setUp() {
         query = new IcebergQuery();
-        List<Aggregation> aggs=  new ArrayList<>();
+        List<Aggregation> aggs = new ArrayList<>();
         List<AggregatorFactory.AggregatorType> opts = new ArrayList<>();
         opts.add(AggregatorFactory.AggregatorType.COUNT);
         opts.add(AggregatorFactory.AggregatorType.SUM);
-        Aggregation agg= new Aggregation();
+        Aggregation agg = new Aggregation();
         agg.setOperators(opts);
         agg.setFieldName("O_TOTALPRICE");
         aggs.add(agg);
@@ -40,7 +40,7 @@ public class IcebergQueryTest {
         sq.setDimension(null);
         sq.setValues(null);
         query.setSelection(sq);
-        List<String> groupFields=  new ArrayList<>();
+        List<String> groupFields = new ArrayList<>();
         groupFields.add("N_NAME");
         groupFields.add("R_NAME");
         query.setGroupFields(groupFields);
@@ -48,27 +48,27 @@ public class IcebergQueryTest {
     }
 
     @Test
-    public void ToStringTest(){
-        String output= query.toString();
-        System.out.println("Iceberg query print to string"+ output);
+    public void ToStringTest() {
+        String output = query.toString();
+        System.out.println("Iceberg query print to string" + output);
     }
 
-    @Test (dataProvider = "IcebergQueryTestDP")
+    @Test(dataProvider = "IcebergQueryTestDP")
     public void ReadTest(String queryPath) throws IOException {
-        String queryFilePath = queryPath; //"../olap-tpch/query.json";
-        FileInputStream fin=new FileInputStream(queryFilePath);
-        IcebergQuery iquery=IcebergQuery.read(fin);
+        String queryFilePath = queryPath; // "../olap-tpch/query.json";
+        FileInputStream fin = new FileInputStream(queryFilePath);
+        IcebergQuery iquery = IcebergQuery.read(fin);
         System.out.println("Read Iceberg query from json" + iquery);
     }
 
     @DataProvider(name = "IcebergQueryTestDP")
     public Object[][] dpArgs() {
         return new Object[][] {
-                {"../datasets/olap-tpch/query.json"}
+                { "../datasets/olap-tpch/query.json" }
         };
     }
 
-    @Test
+    @Test(dependsOnMethods = { "com.nus.cool.functionality.CsvLoaderTest.CsvLoaderUnitTest" })
     public void EcommerceDataTestSQL1() throws Exception {
 
         String dzFilePath = "../CubeRepo";
