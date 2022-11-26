2ddcf4f4f211441ac96f506132a8cd668e34bfa3
qingpeng <qingpeng@u.nus.edu>
2022-11-26 16:31:50 +0800

    Update ProcessorTest

--- cool-core/src/test/java/com/nus/cool/core/refactor/ProcessorTest.java
+++ cool-core/src/test/java/com/nus/cool/core/refactor/ProcessorTest.java
@@ -1,7 +1,5 @@
 package com.nus.cool.core.refactor;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nus.cool.core.cohort.refactor.CohortProcessor;
 import com.nus.cool.core.cohort.refactor.CohortQueryLayout;
 import com.nus.cool.core.cohort.refactor.storage.CohortRet;
@@ -10,9 +8,6 @@ import com.nus.cool.functionality.CsvLoaderTest;
 import com.nus.cool.model.CoolModel;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
@@ -29,9 +24,6 @@ public class ProcessorTest extends CsvLoaderTest {
   private final String cubeRepo = "../CubeRepo/TestCube";
   private CoolModel coolModel;
 
-  private final String queryName = "query.json";
-  private final String resultName = "query_result.json";
-
   @BeforeTest
   public void setUp() {
     logger.info("Start UnitTest " + ProcessorTest.class.getSimpleName());
@@ -47,8 +39,8 @@ public class ProcessorTest extends CsvLoaderTest {
 
   @Test(dataProvider = "ProcessQueryDP", dependsOnMethods = {
       "com.nus.cool.functionality.CsvLoaderTest.csvLoaderUnitTest"})
-  public void processQueryAndValidResult(String queryDir) throws IOException {
-    String queryPath = Paths.get(queryDir, this.queryName).toString();
+  public void processQueryAndValidResult(String queryPath, String queryResultPath)
+      throws IOException {
     CohortQueryLayout layout = CohortQueryLayout.readFromJson(queryPath);
     CohortProcessor cohortProcessor = new CohortProcessor(layout);
 
@@ -56,33 +48,21 @@ public class ProcessorTest extends CsvLoaderTest {
     this.coolModel = new CoolModel(this.cubeRepo);
     coolModel.reload(cohortProcessor.getDataSource());
     CubeRS cube = coolModel.getCube(cohortProcessor.getDataSource());
+    File currentVersion = this.coolModel.getCubeStorePath(cohortProcessor.getDataSource());
+
+    // load input cohort
+    if (cohortProcessor.getInputCohort() != null) {
+      File cohortFile = new File(currentVersion, "cohort/" + cohortProcessor.getInputCohort());
+      if (cohortFile.exists()) {
+        cohortProcessor.readOneCohort(cohortFile);
+      }
+    }
 
     // get current dir path
-    File currentVersion = this.coolModel.loadLatestVersion(cohortProcessor.getDataSource());
     CohortRet ret = cohortProcessor.process(cube);
     String cohortStoragePath = cohortProcessor.persistCohort(currentVersion.toString());
-    cohortProcessor.readExistingCohort(cohortStoragePath);
+    cohortProcessor.readQueryCohorts(cohortStoragePath);
     Assert.assertTrue(cohortProcessor.getPreviousCohortUsers().size() > 0);
-
-    String queryResultPath = Paths.get(queryDir, this.resultName).toString();
-    ObjectMapper mapper = new ObjectMapper();
-    // HashMap<String, List<Integer>> cohortData = mapper.readValue(new
-    // File(queryResultPath), HashMap.class);
-    HashMap<String, List<Integer>> cohortData = mapper.readValue(new File(queryResultPath),
-        new TypeReference<HashMap<String, List<Integer>>>() {
-        });
-    // check the result
-    // System.out.println(ret.toString());
-    // validate the cohortName
-    Assert.assertEquals(ret.getCohortList().size(), cohortData.size());
-
-    // System.out.println(ret.getCohortList());
-    for (String cohortName : ret.getCohortList()) {
-      Assert.assertTrue(cohortData.containsKey(cohortName));
-      // System.out.printf("True Result %s\n", cohortData.get(cohortName).toString());
-      // System.out.printf("Get Result %s\n", ret.getValuesByCohort(cohortName));
-      Assert.assertEquals(cohortData.get(cohortName), ret.getValuesByCohort(cohortName));
-    }
   }
 
   /**
@@ -90,13 +70,10 @@ public class ProcessorTest extends CsvLoaderTest {
    */
   @DataProvider(name = "ProcessQueryDP")
   public Object[][] queryDirDataProvider() {
-    return new Object[][] {{"../datasets/health_raw/sample_query_distinctcount"},
-        {"../datasets/ecommerce_query/sample_query"}, {"../datasets/health_raw/sample_query_count"},
-        {"../datasets/health_raw/sample_query_average"},
-        {"../datasets/health_raw/sample_query_max"}, {"../datasets/health_raw/sample_query_min"},
-        {"../datasets/health_raw/sample_query_sum"},
-        {"../datasets/fraud_case/sample_query_login_count"},
-        {"../datasets/health/sample_query_distinctcount"}};
+    return new Object[][] {
+        // heath_rew
+        {"../datasets/health_raw/sample_query_distinctcount/query.json",
+            "../datasets/health_raw/sample_query_distinctcount/query_result.json"},
+    };
   }
-
 }
