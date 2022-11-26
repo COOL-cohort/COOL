1366cc646b3c0f6a334b5bd726da40bfa047984f
qingpeng <qingpeng@u.nus.edu>
2022-11-26 16:30:06 +0800

    update cohort loader

--- cool-core/src/main/java/com/nus/cool/functionality/CohortLoader.java
+++ cool-core/src/main/java/com/nus/cool/functionality/CohortLoader.java
@@ -19,33 +19,12 @@
 
 package com.nus.cool.functionality;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.nus.cool.core.cohort.CohortAggregation;
-import com.nus.cool.core.cohort.CohortKey;
-import com.nus.cool.core.cohort.CohortQuery;
-import com.nus.cool.core.cohort.CohortSelection;
-import com.nus.cool.core.cohort.QueryResult;
-import com.nus.cool.core.io.compression.SimpleBitSetCompressor;
-import com.nus.cool.core.io.readstore.ChunkRS;
+import com.nus.cool.core.cohort.refactor.CohortProcessor;
+import com.nus.cool.core.cohort.refactor.CohortQueryLayout;
 import com.nus.cool.core.io.readstore.CubeRS;
-import com.nus.cool.core.io.readstore.CubletRS;
-import com.nus.cool.core.io.readstore.MetaChunkRS;
-import com.nus.cool.core.io.readstore.MetaFieldRS;
-import com.nus.cool.core.schema.TableSchema;
-import com.nus.cool.core.util.IntegerUtil;
-import com.nus.cool.core.util.converter.DayIntConverter;
-import com.nus.cool.core.util.converter.NumericConverter;
 import com.nus.cool.model.CoolModel;
-import com.nus.cool.result.ResultTuple;
-import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.BitSet;
-import java.util.List;
-import java.util.Map;
 
 /**
  * CohortLoader is a higher level abstraction for cohort query.
@@ -54,122 +33,34 @@ import java.util.Map;
 public class CohortLoader {
 
   /**
-   * Local model for cool.
+   * Local cohort results for cool.
    *
    * @param args [0] the output data dir (eg, dir of .dz file)
-   *             args [1] application name, also the folder name under above
-   *             folder
-   *             args [2] query's path, eg sogamo/query0.json
+   *             [1] path to the cohort file.
+   *             e.g., datasets/health_raw/sample_query_cohort_loader/query.json
    * @throws IOException when CoolModel throw IOException
    */
   public static void main(String[] args) throws IOException {
+    String cubeRepo = args[0];
+    String queryPath = args[1];
 
-    String datasetPath = args[0];
-    String appPath = args[1];
-    String queryPath = args[2];
+    CohortQueryLayout layout = CohortQueryLayout.readFromJson(queryPath);
+    CohortProcessor cohortProcessor = new CohortProcessor(layout);
 
-    CoolModel coolModel = new CoolModel(datasetPath);
-    coolModel.reload(appPath);
+    // start a new cool model and reload the cube
+    CoolModel coolModel = new CoolModel(cubeRepo);
+    coolModel.reload(cohortProcessor.getDataSource());
+    CubeRS cube = coolModel.getCube(cohortProcessor.getDataSource());
+    File currentVersion = coolModel.getCubeStorePath(cohortProcessor.getDataSource());
 
-    // cohort query from json
-    ObjectMapper mapper = new ObjectMapper();
-    CohortQuery query = mapper.readValue(new File(queryPath), CohortQuery.class);
-
-    System.out.println(" ------  checking query info ------ ");
-    System.out.println(query);
-    System.out.println(" ------  checking query info done ------ ");
-
-    // Load cubes from outsource, which is output of previous query if any
-    Map<String, DataOutputStream> outSourceMap = Maps.newHashMap();
-    if (query.getOutSource() != null) {
-      File root = new File(appPath, query.getOutSource());
-      File[] versions = root.listFiles(File::isDirectory);
-      if (versions != null) {
-        // for each directory
-        for (File version : versions) {
-          File[] cubletFiles = version.listFiles((file, s) -> s.endsWith(".dz"));
-          if (cubletFiles != null) {
-            // for each file under such directory
-            for (File cubletFile : cubletFiles) {
-              outSourceMap.put(cubletFile.getName(),
-                  new DataOutputStream(new FileOutputStream(cubletFile, true)));
-            }
-          }
-        }
+    // load cohort
+    if (cohortProcessor.getInputCohort() != null) {
+      File cohortFile = new File(currentVersion, cohortProcessor.getInputCohort());
+      if (cohortFile.exists()) {
+        cohortProcessor.readOneCohort(cohortFile);
       }
     }
 
-    System.out.println(" ------  checking outSourceMap  ------ ");
-    System.out.println(outSourceMap);
-    System.out.println(" ------  checking outSourceMap done  ------ ");
-
-    List<ResultTuple> resultTuples = executeQuery(
-        coolModel.getCube(query.getDataSource()), query,
-        outSourceMap);
-    QueryResult result = QueryResult.ok(resultTuples);
-    System.out.println(result.toString());
     coolModel.close();
-  }
-
-  /**
-   * Execute query.
-   *
-   * @param cube  the cube that stores the data we need
-   * @param query the cohort query needed to process
-   * @param map   the cublet and it's data
-   * @return the result of the query
-   */
-  public static List<ResultTuple> executeQuery(CubeRS cube, CohortQuery query,
-      Map<String, DataOutputStream> map) throws IOException {
-    List<CubletRS> cublets = cube.getCublets();
-    TableSchema schema = cube.getSchema();
-    List<ResultTuple> resultSet = Lists.newArrayList();
-    boolean tag = query.getOutSource() != null;
-    List<BitSet> bitSets = Lists.newArrayList();
-    // process each cublet
-    for (CubletRS cublet : cublets) {
-      MetaChunkRS metaChunk = cublet.getMetaChunk();
-      // init cohort seleciton
-      CohortSelection sigma = new CohortSelection();
-      // init Aggregation
-      CohortAggregation gamma = new CohortAggregation(sigma);
-      gamma.init(schema, query);
-      gamma.process(metaChunk);
-      if (sigma.isUserActiveCublet()) {
-        List<ChunkRS> dataChunks = cublet.getDataChunks();
-        for (ChunkRS dataChunk : dataChunks) {
-          gamma.process(dataChunk);
-          bitSets.add(gamma.getBs());
-        }
-      }
-      if (tag) {
-        int end = cublet.getLimit();
-        DataOutputStream out = map.get(cublet.getFile());
-        for (BitSet bs : bitSets) {
-          SimpleBitSetCompressor.compress(bs, out);
-        }
-        out.writeInt(IntegerUtil.toNativeByteOrder(end));
-        out.writeInt(IntegerUtil.toNativeByteOrder(bitSets.size()));
-        out.writeInt(IntegerUtil.toNativeByteOrder(0));
-      }
-
-      String cohortField = query.getCohortFields()[0];
-      String actionTimeField = schema.getActionTimeFieldName();
-      NumericConverter converter = cohortField.equals(actionTimeField)
-          ? DayIntConverter.getInstance() : null;
-      MetaFieldRS cohortMetaField = metaChunk.getMetaField(cohortField);
-      Map<CohortKey, Long> results = gamma.getCubletResults();
-      for (Map.Entry<CohortKey, Long> entry : results.entrySet()) {
-        CohortKey key = entry.getKey();
-        int cId = key.getCohort();
-        String cohort = converter == null ? cohortMetaField.getString(key.getCohort())
-            : converter.getString(cId);
-        int age = key.getAge();
-        long measure = entry.getValue();
-        resultSet.add(new ResultTuple(cohort, age, measure));
-      }
-    }
-    // merge the partial query results
-    return ResultTuple.merge(resultSet);
   }
 }
