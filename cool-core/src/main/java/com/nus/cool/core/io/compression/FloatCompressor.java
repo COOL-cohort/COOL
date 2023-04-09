79cc824a2c9340e736c404554ffb81d3891d5d7a
hugy718 <hugy718@gmail.com>
2023-04-09 13:51:47 +0800

    add float compression and decompression (tested)

--- cool-core/src/main/java/com/nus/cool/core/io/compression/FloatCompressor.java
+++ cool-core/src/main/java/com/nus/cool/core/io/compression/FloatCompressor.java
@@ -1,12 +1,16 @@
 package com.nus.cool.core.io.compression;
 
+import com.nus.cool.core.field.FieldValue;
+import com.nus.cool.core.field.RangeField;
 import java.nio.ByteBuffer;
 import java.util.BitSet;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Only accept float (32 bit).
  */
-public class FloatCompressor {
+public class FloatCompressor implements Compressor {
   public static final int HEADACC = 4 + 4;
 
   public static final int maxWidth = 2 + 5 + 6 + 32;
@@ -19,8 +23,8 @@ public class FloatCompressor {
     }
 
     float lastVal;
-    float lastDiffStart;
-    float lastDiffSize;
+    int lastDiffStart;
+    int lastDiffSize;
   }
 
   private static void append(BitSet src, int srcStart, BitSet dest, int destStart, int size) {
@@ -38,7 +42,8 @@ public class FloatCompressor {
     }
     // get the xor value
     int xor = Float.floatToIntBits(f) ^ Float.floatToIntBits(ctx.lastVal);
-    BitSet xorBits = BitSet.valueOf(ByteBuffer.allocate(4).putInt(xor).array());
+    BitSet xorBits = BitSet.valueOf(new long[]{xor});
+    System.out.println(xorBits.toString());
     // get the diff range
     int diffFirst = xorBits.nextSetBit(0);
     int diffLast = xorBits.previousSetBit(xorBits.size());
@@ -46,6 +51,7 @@ public class FloatCompressor {
 
     ctx.lastVal = f;
 
+    dest.set(startOff);
     if (diffSize == ctx.lastDiffSize) {
       // case 2: same xor size
       // set meaningful xor bits
@@ -55,12 +61,12 @@ public class FloatCompressor {
       // case 3: different xor size
       dest.set(startOff + 1);
       // set leading 0 len
-      append(BitSet.valueOf(ByteBuffer.allocate(4).putInt(diffFirst)), 4 * 8 - 5, dest,
+      append(BitSet.valueOf(new long[]{diffFirst}), 0, dest,
           startOff + 2, 5);
       // set meaningful xor bits len
-      append(BitSet.valueOf(ByteBuffer.allocate(4).putInt(diffSize)), 4 * 8 - 6, dest,
-          startOff + 2 + 5, diffSize);
-      // set meaningful xor bits
+      append(BitSet.valueOf(new long[]{diffSize}), 0, dest,
+          startOff + 2 + 5, 6);
+      // set meaningful xor bits    
       append(xorBits, diffFirst, dest, startOff + 2 + 5 + 6, diffSize);
       ctx.lastDiffStart = diffFirst;
       ctx.lastDiffSize = diffSize;
@@ -77,17 +83,49 @@ public class FloatCompressor {
     // generate header and put the first float
     ByteBuffer header = ByteBuffer.allocate(HEADACC);
     header.putInt(floatCount);
+    int numBytePlaceHolder = 0;
+    header.putInt(numBytePlaceHolder);
     final float firstVal = src.getFloat();
     header.putFloat(firstVal);
-
+    header.position(0);
     // create a separate bitset for compression
     BitSet bs = BitSet.valueOf(header);
 
     Context ctx = new Context(firstVal);
-    int curOff = HEADACC * 8;
+    int curOff = (HEADACC + 4) * 8; // the first value
     while (--floatCount > 0) {
-      curOff = add(bs, curOff, src.getFloat(), ctx);
+      curOff += add(bs, curOff, src.getFloat(), ctx);
     }
-    return bs.toByteArray();
+    byte[] ret = bs.toByteArray();
+    header = ByteBuffer.wrap(ret);
+    header.position(4);
+    header.putInt(ret.length - HEADACC);
+    return ret;
+  }
+
+  @Override
+  public CompressorOutput compress(List<? extends FieldValue> src) {
+    int floatCount = src.size();
+    // generate header and put the first float
+    ByteBuffer header = ByteBuffer.allocate(HEADACC + 4);
+    header.putInt(floatCount);
+    int numBytePlaceHolder = 0;
+    header.putInt(numBytePlaceHolder);
+    Iterator<? extends FieldValue> itr = src.iterator();
+    final float firstVal = ((RangeField) itr.next()).getFloat();
+    header.putFloat(firstVal);
+    header.position(0);
+    BitSet bs = BitSet.valueOf(header);
+
+    Context ctx = new Context(firstVal);
+    int curOff = (HEADACC + 4) * 8;
+    while (itr.hasNext()) {
+      curOff += add(bs, curOff, ((RangeField) itr.next()).getFloat(), ctx);
+    }
+    byte[] ret = bs.toByteArray();
+    header = ByteBuffer.wrap(ret);
+    header.position(4);
+    header.putInt(ret.length - HEADACC);
+    return new CompressorOutput(ret, ret.length);
   }
 }
